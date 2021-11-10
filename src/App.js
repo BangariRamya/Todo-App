7fae8dbc60dd8f08af4b37a270ba0d490f3c638d
BangariRamya <92727196+BangariRamya@users.noreply.github.com>
2021-11-10 18:43:48 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+// import logo from './logo.svg';
+import React, { Component } from 'react';
+import './App.css';
+import TodoList from './ToDoList';
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faTrash, faPlus, faEdit } from "@fortawesome/free-solid-svg-icons";
+library.add(faTrash, faEdit, faPlus);
+
+class App extends Component{
+  
+  render() {
+    return(
+      <div>
+       <TodoList/>
+      </div>
+    );
+  }
+}
+
+export default App;
